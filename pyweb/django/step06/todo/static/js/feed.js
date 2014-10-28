e27cd471a53dd13767bc7f0922b51d5728c60a26
nikofu <nikofu501@nikofu501-no-MacBook-Air.local>
2014-10-29 02:44:39 +0900

    hennkou

--- pyweb/django/step06/todo/static/js/feed.js
+++ pyweb/django/step06/todo/static/js/feed.js
@@ -1,5 +1,6 @@
 google.load("feeds", "1");
-function initialize() {
+function initialize(btn_num) {
+    console.log(btn_num);
 
   //フィードURL
   var my_urls = new Array();
@@ -7,6 +8,13 @@ function initialize() {
   my_urls.push('https://www.jpcert.or.jp/rss/jpcert.rdf');
   my_urls.push('http://blog.security.biglobe.ne.jp/rss/index.rdf');
   //my_urls.push();
+
+  var my_author = new Array();
+  my_author.push('艦これ');
+  my_author.push('JPCERT');
+  my_author.push('BIGLOBE セキュリティ');
+  //my_author[''] = "";
+
 
   var num = 15; //フィードを読み込む数
   var content = document.getElementById('feed');//フィード表示場所のID名
@@ -20,6 +28,7 @@ function initialize() {
     for (var feed_num = 0; feed_num < my_urls.length; feed_num++) {
         var url = my_urls[feed_num];
 
+          var html_author = "";
           //キャッシュ対策
           var d = new Date();
           var q = d.getMonth()+""+d.getDate()+""+d.getHours();
@@ -50,33 +59,46 @@ function initialize() {
                 all_feeds += 1;
 
                 //my_object[i] = date + '<p><a href="' + link + '">' + title + '</a></p>';
-                my_object[date + " " + title] = '<p><a href="' + link + '">' + title + '</a></p>';
-                my_day[date + " " + title] = date;
-                my_title_object.push(date + " " + title);
+                if(btn_num == 1){
+                    my_object[date + " " + title] = '<p><a href="' + link + '">' + title + '</a></p>';
+                    my_day[date + " " + title] = date;
+                    my_title_object.push(date + " " + title);
+                }else{
+                    html_author += date + '<p><a href="' + link + '">' + title + '</a></p>';
+                }
 
                 //html += '<p><a href="' + link + '">' + date + title + '</a></p>';
                 //html += my_object[i];
-                }
-              //content.innerHTML = html;　//指定したID内に出力
+              }
+
+              if(btn_num != 1){
+                html_author += '<br><br>';
+                html_author += '<h1>' + my_author[feed_num] + '</h1>';
+                content.innerHTML = html_author;
+              }
+
             }else{
               alert(result.error.code + ":" + result.error.message);
             }
 
-            my_title_object = my_title_object.sort();
             var day_display = 0;
-            var html =""; //表示用HTMLの準備
+            var html_new =""; //表示用HTMLの準備
+            if(btn_num == 1){
+                my_title_object = my_title_object.sort();
+                my_title_object = my_title_object.reverse();
 
-            for(var i = 0; i < my_title_object.length; i++) {
-                if(day_display != my_day[my_title_object[i]]){
-                    day_display = my_day[my_title_object[i]];
-                    html += day_display;
+                for(var i = 0; i < my_title_object.length; i++) {
+                    if(day_display != my_day[my_title_object[i]]){
+                        day_display = my_day[my_title_object[i]];
+                        html_new += day_display;
+                    }
+
+                    console.log(my_title_object[i]);
+                    html_new += my_object[my_title_object[i]];
                 }
-
-                console.log(my_title_object[i]);
-                html += my_object[my_title_object[i]];
+                console.log("all " ,all_feeds);
+                content.innerHTML = html_new;　//指定したID内に出力
             }
-            console.log("all " ,all_feeds);
-            content.innerHTML = html;　//指定したID内に出力
           });
      }
 }
